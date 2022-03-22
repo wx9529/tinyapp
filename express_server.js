6a386fc137e188f937ecd83e8702f1022c0e4f86
wangxin <xinwang213z@gmail.com>
2022-03-22 00:09:10 +0000

    Set ejs as the view engine

--- express_server.js
+++ express_server.js
@@ -1,24 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-}
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+};
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello!");
-})
+});
 
-app.get('/urls.json', (req, res) => {
+app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-})
-app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
-})
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-})
+});
